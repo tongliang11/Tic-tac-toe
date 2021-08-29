fefa4436b9171636dc8ab350652e46608a29bfdc
Tong Liang <tong.liang@stonybrook.edu>
2021-08-28 23:53:20 -0400

    implement evolutionary algorithm

--- tic-tac-toe/computer_play.py
+++ tic-tac-toe/computer_play.py
@@ -1,5 +1,50 @@
-import numpy as np
-def move_random(game_state):
+import random
+import time
+# import tic_tac_toe
+from utility import is_winner
+
+def move_random(game_state, delay=0.5):
     empty_locations = [
-        i+1 for i in range(len(game_state)) if game_state[i] == ' ']
-    return np.random.choice(empty_locations)
+        i for i in range(len(game_state)) if game_state[i] == ' ']
+    time.sleep(delay)  # fake computer thinking delay
+    return random.choice(empty_locations)+1
+
+
+def move_evolutionary(game_state, computer_label='O', rounds=1000):
+    empty_locations = [
+        i for i in range(len(game_state)) if game_state[i] == ' ']
+    if empty_locations.__len__() == 0:
+        return -1
+    else:
+        winning_prob = []
+        for next_move in empty_locations:
+            _game_state = list(game_state)
+            _game_state[next_move] = computer_label
+            winner = []
+            for i in range(rounds):
+                winner.append(_random_self_play(list(_game_state)))
+
+            winning_prob.append(
+                len([_ for _ in winner if _ == computer_label])/rounds)
+
+        return empty_locations[max(range(len(empty_locations)), key=lambda x:winning_prob[x])]+1
+
+
+def _random_self_play(game_state, label_computer1='X'):
+    label_computer2 = 'O' if label_computer1 == 'X' else 'X'
+    while True:
+
+        computer_location1 = move_random(game_state, delay=0)
+        game_state[computer_location1-1] = label_computer1
+
+        if is_winner(game_state):
+            return is_winner(game_state)
+        computer_location2 = move_random(game_state, delay=0)
+        game_state[computer_location2-1] = label_computer2
+
+        if is_winner(game_state):
+            return is_winner(game_state)
+
+
+if __name__ == '__main__':
+    print(move_evolutionary(['O', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ']))
